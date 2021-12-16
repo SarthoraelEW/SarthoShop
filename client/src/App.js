b0ae52fc1ab12ef01a7f5510901062e28290c449
SarthoraelEW <sarthoraelew@gmail.com>
2021-12-16 17:50:15 +0100

    create client side

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import Routes from "./components/Routes/Index";
+import { UidContext } from "./components/AppContext";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [uid, setUid] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true
+      })
+      .then((res) => setUid(res.data))
+      .catch((err) => console.log("No token"));
+    }
+    fetchToken();
+  }, [uid]);
+
+  return (
+    <UidContext.Provider value={uid}>
+      <Routes />
+    </UidContext.Provider>
+  );
+}
+
+export default App;
